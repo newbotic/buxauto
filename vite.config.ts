b610bbafcf9aef1ba6ba4f88830877170196b7af
daso daso <like_plus@yahoo.es>
2025-10-24 20:32:42 +0300

    update vit config

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/buxauto/', // ✅ Must match your repo name exactly
+  base: '/buxauto/', // ✅ must match your repo name
 })
